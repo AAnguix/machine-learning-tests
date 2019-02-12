6c94a058a67dfc5a425fefa3894e38174d5767f7
Arturo <arturo.anguix.l@gmail.com>
2019-02-12 20:39:39 +0100

    Update comments in predict

--- predict.m
+++ predict.m
@@ -1,32 +1,32 @@
-function p = predict(Theta1, Theta2, X)
-%PREDICT Predict the label of an input given a trained neural network
-%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
-%   trained weights of a neural network (Theta1, Theta2)
+% Returns the predicted label of an input given the parameters/weights of a trained neural network.
+% theta1: Weights of the hidden layer.
+% theta2: Weights of the output layer.
+function predictedLabel = predict(theta1, theta2, X)
 
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+trainingExamples = size(X, 1);
+num_labels = size(theta2, 1);
 
-p = zeros(size(X, 1), 1);
+predictedLabel = zeros(size(X, 1), 1);
 
-% Add ones to the X data matrix (a_0_1)
-a1 = [ones(m, 1) X];
+% Add the bias term(a_0_1) to the X data matrix 
+a1 = [ones(trainingExamples, 1) X];
 
 %Compute the hidden layer
-z2 = z2(Theta1, a1);
+z2 = z2(theta1, a1);
 a2 = sigmoid(z2);
 
-% Add ones to the a2 data matrix (a_0_2)
+% Add the bias term(a_0_2) to the a2 data matrix 
 a2Cols = size(a2, 1);
 a2 = [ones(a2Cols, 1) a2];
 
 %Compute the output layer
-z3 = z3(Theta2, a2);
+z3 = z3(theta2, a2);
 a3 = sigmoid(z3);
 
-% Pick the max value of the 10 columns for each row.
+% Pick the maximum value of all the columns for each row.
 % The column indicates the label (predicted value). 
 [maxPrediction, maxIndex] = max(a3, [], 2);
-p = maxIndex;
+predictedLabel = maxIndex;
 end
 
 function z2 = z2(theta1, a1)
